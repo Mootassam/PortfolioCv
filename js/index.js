dc0dfe06a72076ff149a4a93e853b4045d184f54
Mootassam <mootassame@gmail.com>
2022-07-01 19:17:35 -0400

    delet the audio

--- js/index.js
+++ js/index.js
@@ -1,21 +1,6 @@
-var audio = document.getElementById("my_audio");
-var stop = document.getElementById("stop");
-var play = document.getElementById("play");
 const links = document.querySelector(".header__links");
 const links__ul = document.querySelector("#links__ul");
 const header = document.querySelector(".header");
-play.style.display = "none";
-function playAudio() {
-  audio.play();
-  play.style.display = "block";
-  stop.style.display = "none";
-}
-
-function pauseAudio() {
-  audio.pause();
-  play.style.display = "none";
-  stop.style.display = "block";
-}
 
 window.matchMedia("min-width:1024px").addEventListener("change", function () {
   links.style = " display:flex";
